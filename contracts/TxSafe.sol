a56218e2b3f26d8592109d7d2f0edb99a996882a
slabbaki <sami.labbaki@keble.ox.ac.uk>
2023-07-22 22:17:56 +0200

    add all basic functions

--- contracts/TxSafe.sol
+++ contracts/TxSafe.sol
@@ -21,6 +21,7 @@ interface IERC20 {
 
 contract TxSafe {
 
+    IERC20 public TXS;
     IERC20 public stNATIVE;
     IERC20 public DAI;
 
@@ -29,38 +30,56 @@ contract TxSafe {
     AggregatorV3Interface internal priceFeed;
 
     // State Variables
+    uint txs_price_pool0 = 1000000000000000000;
+    uint txs_price_pool1 = 1000000000000000000;
+    uint txs_price_pool2 = 1000000000000000000;
+
     uint pool0_min_value = 0;
     uint pool1_min_value = 0;
     uint pool2_min_value = 0;
 
     uint public coverages_count = 0;
     uint public deposits_count = 0;
-    uint public claims_count = 0;
-    uint public claims_resolved_count = 0;
 
     uint public pool0_native_value = 0;
     uint public pool1_native_value = 0;
     uint public pool2_native_value = 0;
+    
+    uint public pool0_stnative_value = 0;
+    uint public pool1_stnative_value = 0;
+    uint public pool2_stnative_value = 0;
 
     uint public pool0_stable_value = 0;
     uint public pool1_stable_value = 0;
     uint public pool2_stable_value = 0;
 
-    mapping(address => mapping(string => mapping(string => uint))) public users_coverage;
+    mapping(address => mapping(string => mapping(string => mapping(uint => mapping(uint => uint))))) public users_coverage;
     mapping(address => mapping(string => mapping(uint => mapping(uint8 => uint)))) public users_deposits;
     mapping(address => mapping(string => mapping(uint => mapping(uint8 => uint)))) public users_withdrawals;
+    
     mapping(address => uint) public users_deposit_native_value;
     mapping(address => uint) public users_deposit_stnative_value;
     mapping(address => uint) public users_deposit_dai_value;
+
+    mapping(address => uint) public users_native_txs_holding_pool0;
+    mapping(address => uint) public users_native_txs_holding_pool1;
+    mapping(address => uint) public users_native_txs_holding_pool2;
+    mapping(address => uint) public users_stnative_txs_holding_pool0;
+    mapping(address => uint) public users_stnative_txs_holding_pool1;
+    mapping(address => uint) public users_stnative_txs_holding_pool2;
+    mapping(address => uint) public users_dai_txs_holding_pool0;
+    mapping(address => uint) public users_dai_txs_holding_pool1;
+    mapping(address => uint) public users_dai_txs_holding_pool2;
 
     modifier onlyOwner() {
         require(msg.sender == owner);
         _;
     }
 
-    constructor(address _stNative, address _dai, address _feed) {
+    constructor(address _txs, address _stNative, address _dai, address _feed) {
         owner = msg.sender;
 
+        TXS = IERC20(_txs);
         stNATIVE = IERC20(_stNative);
         DAI = IERC20(_dai);
         priceFeed = AggregatorV3Interface(_feed);
@@ -78,6 +97,18 @@ contract TxSafe {
 
     function change_pool2_min_value(uint _value) onlyOwner public {
         pool2_min_value = _value;
+    }
+
+    function update_txs_price_pool0(uint _new_txs_price) onlyOwner public {
+        txs_price_pool0 = _new_txs_price;
+    }
+
+    function update_txs_price_pool1(uint _new_txs_price) onlyOwner public {
+        txs_price_pool1 = _new_txs_price;
+    }
+
+    function update_txs_price_pool2(uint _new_txs_price) onlyOwner public {
+        txs_price_pool2 = _new_txs_price;
     }
 
     // ORACLE ETH price
@@ -106,10 +137,19 @@ contract TxSafe {
 
         if (_risk_pool == 0) {
             pool0_native_value += _amount;
+            users_native_txs_holding_pool0[msg.sender] += _amount * (10 ** 18) * uint(getLatestPrice()) / txs_price_pool0;
+            (bool sent) = TXS.transfer(msg.sender, _amount * (10 ** 18) * uint(getLatestPrice()) / txs_price_pool0);
+            require(sent, "Failed to unstake unlocked tokens");
         } else if (_risk_pool == 1) {
             pool1_native_value += _amount;
+            users_native_txs_holding_pool1[msg.sender] += _amount * (10 ** 18) * uint(getLatestPrice()) / txs_price_pool1;
+            (bool sent) = TXS.transfer(msg.sender, _amount * (10 ** 18) * uint(getLatestPrice()) / txs_price_pool1);
+            require(sent, "Failed to unstake unlocked tokens");
         } else if (_risk_pool == 2) {
             pool2_native_value += _amount;
+            users_native_txs_holding_pool2[msg.sender] += _amount * (10 ** 18) * uint(getLatestPrice()) / txs_price_pool2;
+            (bool sent) = TXS.transfer(msg.sender, _amount * (10 ** 18) * uint(getLatestPrice()) / txs_price_pool2);
+            require(sent, "Failed to unstake unlocked tokens");
         }
     }
 
@@ -123,11 +163,20 @@ contract TxSafe {
             require(_success);
             users_deposit_stnative_value[msg.sender] += _amount;
             if (_risk_pool == 0) {
-                pool0_native_value += _amount;
+                pool0_stnative_value += _amount;
+                users_stnative_txs_holding_pool0[msg.sender] += _amount * (10 ** 18) * uint(getLatestPrice()) / txs_price_pool0;
+                (bool sent) = TXS.transfer(msg.sender, _amount * (10 ** 18) * uint(getLatestPrice()) / txs_price_pool0);
+                require(sent, "Failed to unstake unlocked tokens");
             } else if (_risk_pool == 1) {
-                pool1_native_value += _amount;
+                pool1_stnative_value += _amount;
+                users_stnative_txs_holding_pool1[msg.sender] += _amount * (10 ** 18) * uint(getLatestPrice()) / txs_price_pool1;
+                (bool sent) = TXS.transfer(msg.sender, _amount * (10 ** 18) * uint(getLatestPrice()) / txs_price_pool1);
+                require(sent, "Failed to unstake unlocked tokens");
             } else if (_risk_pool == 2) {
-                pool2_native_value += _amount;
+                pool2_stnative_value += _amount;
+                users_stnative_txs_holding_pool2[msg.sender] += _amount * (10 ** 18) * uint(getLatestPrice()) / txs_price_pool2;
+                (bool sent) = TXS.transfer(msg.sender, _amount * (10 ** 18) * uint(getLatestPrice()) / txs_price_pool2);
+                require(sent, "Failed to unstake unlocked tokens");
             }
         } else if (keccak256(bytes(_token)) == keccak256(bytes("DAI"))) {
             bool _success = DAI.transferFrom(msg.sender, address(this), _amount);
@@ -135,10 +184,19 @@ contract TxSafe {
             users_deposit_dai_value[msg.sender] += _amount;
             if (_risk_pool == 0) {
                 pool0_stable_value += _amount;
+                users_dai_txs_holding_pool0[msg.sender] += _amount * (10 ** 18) / txs_price_pool0;
+                (bool sent) = TXS.transfer(msg.sender, _amount * (10 ** 18) / txs_price_pool0);
+                require(sent, "Failed to unstake unlocked tokens");
             } else if (_risk_pool == 1) {
                 pool1_stable_value += _amount;
+                users_dai_txs_holding_pool1[msg.sender] += _amount * (10 ** 18) / txs_price_pool1;
+                (bool sent) = TXS.transfer(msg.sender, _amount * (10 ** 18) / txs_price_pool1);
+                require(sent, "Failed to unstake unlocked tokens");
             } else if (_risk_pool == 2) {
                 pool2_stable_value += _amount;
+                users_dai_txs_holding_pool2[msg.sender] += _amount * (10 ** 18) / txs_price_pool2;
+                (bool sent) = TXS.transfer(msg.sender, _amount * (10 ** 18) / txs_price_pool2);
+                require(sent, "Failed to unstake unlocked tokens");
             }
         }
         
@@ -151,11 +209,11 @@ contract TxSafe {
         require(_risk_pool == 0 || _risk_pool == 1 || _risk_pool == 2);
         uint pool_value;
         if (_risk_pool == 0) {
-            pool_value = pool0_native_value * uint(getLatestPrice()) + pool0_stable_value;
+            pool_value = pool0_native_value * uint(getLatestPrice()) + pool0_stnative_value * uint(getLatestPrice()) + pool0_stable_value;
         } else if (_risk_pool == 1) {
-            pool_value = pool1_native_value * uint(getLatestPrice()) + pool1_stable_value;
+            pool_value = pool1_native_value * uint(getLatestPrice()) + pool1_stnative_value * uint(getLatestPrice()) + pool1_stable_value;
         } else if (_risk_pool == 2) {
-            pool_value = pool2_native_value * uint(getLatestPrice()) + pool2_stable_value;
+            pool_value = pool2_native_value * uint(getLatestPrice()) + pool2_stnative_value * uint(getLatestPrice()) + pool2_stable_value;
         }
         return pool_value;
     }
@@ -173,106 +231,145 @@ contract TxSafe {
         return pool_min_value;
     }
 
-    function get_native_premium() public pure returns(uint) {
-        uint premium = 0;
+    function get_native_withdrawal_value(uint _amount, uint8 _risk_pool) public view returns(uint) {
+        uint premium;
+        if (_risk_pool == 0) {
+            premium = _amount * txs_price_pool0 / ((10 ** 18) * uint(getLatestPrice()));
+        } else if (_risk_pool == 1) {
+            premium = _amount * txs_price_pool1 / ((10 ** 18) * uint(getLatestPrice()));
+        } else if (_risk_pool == 2) {
+            premium = _amount * txs_price_pool2 / ((10 ** 18) * uint(getLatestPrice()));
+        }
         return premium;
     }
 
-    function get_stable_premium() public pure returns(uint) {
-        uint premium = 0;
+    function get_stable_withdrawal_value(uint _amount, uint8 _risk_pool) public view returns(uint) {
+        uint premium;
+        if (_risk_pool == 0) {
+            premium = _amount * txs_price_pool0 / (10 ** 18);
+        } else if (_risk_pool == 1) {
+            premium = _amount * txs_price_pool1 / (10 ** 18);
+        } else if (_risk_pool == 2) {
+            premium = _amount * txs_price_pool2 / (10 ** 18);
+        }
         return premium;
     }
 
+    // frontend send converted value in TXS
     function withdraw_deposit_native(uint _amount, uint8 _risk_pool, uint _time_in) public {
-
-        require(users_deposit_native_value[msg.sender] >= _amount);
         require(get_pool_value(_risk_pool) - _amount >= get_pool_min_value(_risk_pool));
 
         users_withdrawals[msg.sender]["NATIVE"][_time_in][_risk_pool] = _amount;
         users_deposit_native_value[msg.sender] -= _amount;
 
         if (_risk_pool == 0) {
+            require(users_native_txs_holding_pool0[msg.sender] >= _amount);
             pool0_native_value -= _amount;
+            users_native_txs_holding_pool0[msg.sender] -= _amount;
         } else if (_risk_pool == 1) {
+            require(users_native_txs_holding_pool1[msg.sender] >= _amount);
             pool1_native_value -= _amount;
+            users_native_txs_holding_pool1[msg.sender] -= _amount;
         } else if (_risk_pool == 2) {
+            require(users_native_txs_holding_pool2[msg.sender] >= _amount);
             pool2_native_value -= _amount;
+            users_native_txs_holding_pool2[msg.sender] -= _amount;
         }
 
-        (bool sent, ) = msg.sender.call{value: _amount + get_native_premium()}("");
+        bool _success = TXS.transferFrom(msg.sender, address(this), _amount);
+        require(_success, "Failed to unstake unlocked tokens");
+        (bool sent, ) = msg.sender.call{value: get_native_withdrawal_value(_amount, _risk_pool)}("");
         require(sent, "Failed to unstake unlocked tokens");
     }
 
     function withdraw_deposit(string memory _token, uint _amount, uint8 _risk_pool, uint _time_in) public {
 
         if (keccak256(bytes(_token)) == keccak256(bytes("stNATIVE"))) {
-            require(users_deposit_stnative_value[msg.sender] >= _amount);
             require(get_pool_value(_risk_pool) - _amount >= get_pool_min_value(_risk_pool));
             users_withdrawals[msg.sender]["_token"][_time_in][_risk_pool] = _amount;
             users_deposit_stnative_value[msg.sender] -= _amount;
             if (_risk_pool == 0) {
+                require(users_stnative_txs_holding_pool0[msg.sender] >= _amount);
                 pool0_native_value -= _amount;
+                users_stnative_txs_holding_pool0[msg.sender] -= _amount;
             } else if (_risk_pool == 1) {
+                require(users_stnative_txs_holding_pool1[msg.sender] >= _amount);
                 pool1_native_value -= _amount;
+                users_stnative_txs_holding_pool1[msg.sender] -= _amount;
             } else if (_risk_pool == 2) {
+                require(users_stnative_txs_holding_pool2[msg.sender] >= _amount);
                 pool2_native_value -= _amount;
+                users_stnative_txs_holding_pool2[msg.sender] -= _amount;
             }
 
-            (bool sent) = stNATIVE.transfer(msg.sender, _amount + get_native_premium());
+            bool _success = TXS.transferFrom(msg.sender, address(this), _amount);
+            require(_success, "Failed to unstake unlocked tokens");
+            (bool sent) = stNATIVE.transfer(msg.sender, get_native_withdrawal_value(_amount, _risk_pool));
             require(sent, "Failed to unstake unlocked tokens");
         } else if (keccak256(bytes(_token)) == keccak256(bytes("DAI"))) {
-            require(users_deposit_dai_value[msg.sender] >= _amount);
             require(get_pool_value(_risk_pool) - _amount >= get_pool_min_value(_risk_pool));
             users_withdrawals[msg.sender]["_token"][_time_in][_risk_pool] = _amount;
             users_deposit_dai_value[msg.sender] -= _amount;
             if (_risk_pool == 0) {
+                require(users_dai_txs_holding_pool0[msg.sender] >= _amount);
                 pool0_stable_value -= _amount;
+                users_stnative_txs_holding_pool0[msg.sender] -= _amount;
             } else if (_risk_pool == 1) {
+                require(users_dai_txs_holding_pool1[msg.sender] >= _amount);
                 pool1_stable_value -= _amount;
+                users_stnative_txs_holding_pool1[msg.sender] -= _amount;
             } else if (_risk_pool == 2) {
+                require(users_dai_txs_holding_pool2[msg.sender] >= _amount);
                 pool2_stable_value -= _amount;
+                users_stnative_txs_holding_pool2[msg.sender] -= _amount;
             }
 
-            (bool sent) = DAI.transfer(msg.sender, _amount + get_stable_premium());
+            bool _success = TXS.transferFrom(msg.sender, address(this), _amount);
+            require(_success, "Failed to unstake unlocked tokens");
+            (bool sent) = DAI.transfer(msg.sender, get_stable_withdrawal_value(_amount, _risk_pool));
             require(sent, "Failed to unstake unlocked tokens");
         }
     }
 
     // Coverage Functions
     
-    function get_coverage_price() private view returns (uint) {
-
+    function get_native_coverage_price() private pure returns (uint) {
+        return 0;
     }
 
-    function get_coverage_native(string memory _token, uint _amount, string memory _period) public payable {
-        require(msg.value >0);
-        require(msg.value >= get_coverage_price());
-        require(keccak256(bytes(_token)) == keccak256(bytes("ETH")) || keccak256(bytes(_token)) == keccak256(bytes("DAI")));
-        require(_amount > 0);
-        // require(_period) is correct;
+    function get_coverage_price() private pure returns (uint) {
+        return 0;
+    }
 
-        users_coverage[msg.sender][_token][_period] = _amount;
+    // 1inch, makerdao, uniswap, aave
+    function get_coverage_native(uint _amount, uint _time_in, uint _time_out, string memory _protocols) public payable {
+        require(_amount > 0);
+        require(msg.value > 0);
+        require(_time_in >= block.timestamp);
+        require(_time_in < _time_out);
+        require(msg.value >= get_native_coverage_price());
+
+        users_coverage[msg.sender]["NATIVE"][_protocols][_time_in][_time_out] = _amount;
         coverages_count += 1;
     }
 
-    function get_coverage(string memory _token, uint _amount, string memory _period) public payable {
-        require(msg.value >0);
-        require(msg.value >= get_coverage_price());
-        require(keccak256(bytes(_token)) == keccak256(bytes("ETH")) || keccak256(bytes(_token)) == keccak256(bytes("DAI")));
+    function get_coverage(string memory _token, uint _amount, uint _time_in, uint _time_out, string memory _protocols) public payable {
+        require(keccak256(bytes(_token)) == keccak256(bytes("stNATIVE")) || keccak256(bytes(_token)) == keccak256(bytes("DAI")));
         require(_amount > 0);
-        // require(_period) is correct;
 
-        users_coverage[msg.sender][_token][_period] = _amount;
+        if (keccak256(bytes(_token)) == keccak256(bytes("stNATIVE"))) {
+            bool _success = stNATIVE.transferFrom(msg.sender, address(this), get_native_coverage_price());
+            require(_success);
+        } else if (keccak256(bytes(_token)) == keccak256(bytes("DAI"))) {
+            bool _success = DAI.transferFrom(msg.sender, address(this), get_coverage_price());
+            require(_success);
+            users_deposit_dai_value[msg.sender] += _amount;
+        }
+
+        users_coverage[msg.sender][_token][_protocols][_time_in][_time_out] = _amount;
         coverages_count += 1;
     }
 
-    // Claim Functions
-    // funciton claim() public {
+    //    Viewer functions
 
-    // }
-    
-    // Resolve Claims
-    // function resolve() {
-
-    // }
 }
